9c3ebdc76b98abf5d3467036f3cfe35d6064f762
Kate <rpakreeva@yandex.ru>
2023-05-11 02:34:48 +0300

    add abs sub two digits

--- long_arithmetic/long_arithmetic.cpp
+++ long_arithmetic/long_arithmetic.cpp
@@ -8,9 +8,9 @@ int main()
     //std::string tmp;
     //std::cin >> tmp;
 
-    Value* data = new Value("9999");
-    Value* data1 = new Value("111");
-    Value* data3 = abs_sum(data, data1);
+    Value* data = new Value("111");
+    Value* data1 = new Value("9999");
+    Value* data3 = abs_sub(data, data1);
 
     std::copy(data3->data.begin(), data3->data.end(), std::ostream_iterator<int>(std::cout, " "));
 }
