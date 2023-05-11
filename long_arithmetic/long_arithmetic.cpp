7df7b267ecf627e1d74308089e9c8a3b8d701fe8
Kate <rpakreeva@yandex.ru>
2023-05-12 00:13:08 +0300

    add ref input output

--- long_arithmetic/long_arithmetic.cpp
+++ long_arithmetic/long_arithmetic.cpp
@@ -5,37 +5,15 @@
 
 int main(int count, char* params[])
 {
-    if (count == 3)
-    {
-        Value* data = new Value(params[1]);
-        Value* data1 = new Value(params[2]);
+    Value* data = new Value(params[1]);
+    Value* data1 = new Value(params[2]);
 
-        Value* data4 = summ(data, data1);
-        std::cout << "SUMM :";
-        data4->print();
+    Value* data4 = summ(data, data1);
+    std::cout << "SUMM :";
+    data4->print();
 
-        Value* data3 = subb(data, data1);
-        std::cout << "SUBB :";
-        data3->print();
+    Value* data3 = subb(data, data1);
+    std::cout << "SUBB :";
+    data3->print();
 
-    }
-    else
-    {
-        std::string tmp1;
-        std::cin >> tmp1;
-
-        std::string tmp2;
-        std::cin >> tmp2;
-
-        Value* data = new Value(tmp1);
-        Value* data1 = new Value(tmp2);
-
-        Value* data4 = summ(data, data1);
-        std::cout << "SUMM :";
-        data4->print();
-
-        Value* data3 = subb(data, data1);
-        std::cout << "SUBB :";
-        data3->print();
-    }
 }
